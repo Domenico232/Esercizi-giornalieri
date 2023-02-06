b80be845c1820f71cce672d51d360cac4b631c84
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-02-06 16:07:15 +0100

    Week2/day1

---
+++ week 2/g1/assets/JS/script.js
@@ -0,0 +1,20 @@
+document.getElementById('body').innerHTML = '<header><h2>Iniziamo la nostra pratica</h2><br><p>baolo</p></header>';
+
+window.alert('boh');
+alert('si no 🤔');
+
+document.write ('pantera scritto da javascript');
+var Pantera ='panterona';
+var Lince ='Linciona'
+var hippo ='elefante.tandem.psichico.da.guerra'
+
+console.log(Pantera);
+console.log(Lince);
+console.log(hippo);
+
+
+var out = 'ciccio spar';
+document.getElementById('divvo').innerHTML=out;
+
+numero = 5;
+document.getElementById('numero').innerHTML = numero;
