8d0d4e2b5120fb3cae79ffe24df258b6ff16fd9f
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-02-07 15:57:53 +0100

    Week2/day2/ex

---
+++ week 2/g2/assets/JS/script.js
@@ -0,0 +1,43 @@
+
+const Pgreco = 3.14;
+var rio = 'Aldo';
+let numero =15;
+
+
+{
+    let = client = 'Cliente';
+    document.getElementById('concatena').innerHTML = rio + ', ' + client + ', ' + Pgreco;
+
+    var insieme = rio + ', ' + client + ', ' + Pgreco;
+    document.getElementById('concatena2').innerHTML = insieme;
+}
+
+{
+    let nome = 'mario';
+    let nome2 = 'carla';
+
+    document.getElementById('var').innerHTML = nome;
+    document.getElementById('let').innerHTML = nome2;
+    document.getElementById('final').innerHTML = nome;
+
+    document.getElementById('let2').innerHTML = nome;
+    document.getElementById('let3').innerHTML = nome2;
+    document.getElementById('final2').innerHTML = nome2;
+}
+
+
+    
+    let somma=numero+15;
+    let incre=somma++; 
+    let sott=numero-10;
+    let decre=sott--;
+    let div=numero/3;
+    document.getElementById('iniziale').innerHTML='Valore iniziale: ' +numero;
+    document.getElementById('valore1').innerHTML='Addizione e incremento: ' +incre +', '+somma;
+    document.getElementById('valore2').innerHTML='Sottrazione e decremento'+decre+', '+sott;
+    document.getElementById('valore3').innerHTML='Moltiplicazione: ' +numero*3;
+    document.getElementById('valore4').innerHTML='Divisione: ' +div;
+    document.getElementById('valore5').innerHTML+=+ numero +' è un numero';
+    
+    
+
