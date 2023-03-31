57581068635875a95033b6fa27e93f81829868c3
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-03-31 17:06:30 +0200

    .

--- U3React/Week-proj/proje-weep/src/App.js
+++ U3React/Week-proj/proje-weep/src/App.js
@@ -8,8 +8,15 @@ function App() {
       <header className="App-header">
         <Navbarre />
       </header>
+      <div><h1 className='d-inline pe-2'>TV Shows</h1><select className='bg-dark text-white'>
+        <option>Genres</option>
+        <option> Fantasy</option>
+        <option>Science</option>
+        <option>Romance</option>
+        <option>Comic</option>
+        </select></div>
       <div>
-        <FirstCarousel></FirstCarousel>
+        <FirstCarousel Url="https://www.omdbapi.com/?i=tt3896198&apikey=d3e16d80&s=harry%20potter"/>
       </div>
     </div>
   );
--- U3React/Week-proj/proje-weep/src/components/FirstCarousel.jsx
+++ U3React/Week-proj/proje-weep/src/components/FirstCarousel.jsx
@@ -6,33 +6,55 @@ class FirstCarousel extends Component{
         films:[]
     }
 
+    
+
+    getHarryfilms = async() =>  {
+            try {
+               const response = await fetch({})
+               const Harrymovies = await response.json()
+               console.log(Harrymovies)
+               this.setState({
+                films : Harrymovies.Search
+               })
+            } catch (error) {
+                console.log(error)
+            }
+    }
+    componentDidMount(){
+        this.getHarryfilms()
+    }
+
     render(){
+        
         return (
-            <div>(
-                <Carousel fade>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src="holder.js/800x400?text=First slide&bg=373940"
-                      alt="First slide"
-                    />
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src="holder.js/800x400?text=Second slide&bg=282c34"
-                      alt="Second slide"
-                    />
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src="holder.js/800x400?text=Third slide&bg=20232a"
-                      alt="Third slide"
-                    />
-                  </Carousel.Item>
-                </Carousel>
-              );</div>
+            <div className="pt-5">
+                <h2>Serie del momento </h2>
+                <div> (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );</div>
+              </div>    
         )
     }
 }
