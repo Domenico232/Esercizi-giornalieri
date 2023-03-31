10b8fd75d7246c757d672d04ba74e268ee588c48
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-03-31 18:25:28 +0200

    .

--- U3React/Week-proj/proje-weep/src/components/FirstCarousel.jsx
+++ U3React/Week-proj/proje-weep/src/components/FirstCarousel.jsx
@@ -1,10 +1,33 @@
 import { Component } from "react";
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 class FirstCarousel extends Component{
     state = {
         films:[]
     }
+
+   
   
     
 
@@ -27,39 +50,22 @@ class FirstCarousel extends Component{
     render(){
         
         return (
-            <div className="pt-5">
-                <h2>Serie del momento </h2>
-                <div> (
-    <Carousel>
-       
-        
-        <Carousel.Item>
-        
-            <div className="d-flex">
-            {this.state.films.map((e)=>{
-            return(
-                <>
-        <img
-          className="d-block w-100"
-          src={e.Poster}
-          alt="First slide"
-        />
-        </>)
-        })}
+       <div className="mt-5">
+        <h2>Serie del momento</h2>
+        <Carousel responsive={responsive}>
 
+            {this.state.films.map((e)=>{
+                    return(
+                    <div><img src={e.Poster} alt="" /></div>
+                    )
+            })}
+          
+          
+        </Carousel>
         </div>
-        </Carousel.Item>
-      
-                
-            
-    
         
-      
-    </Carousel>
-  );</div>
-              </div>    
         )
+        }
     }
-}
 
 export default FirstCarousel
