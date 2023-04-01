3daeeb9f6b131296799410ca341148f6cfa7415d
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-01 08:26:38 +0200

    .

--- U3React/Week-proj/proje-weep/src/components/Navbarre.jsx
+++ U3React/Week-proj/proje-weep/src/components/Navbarre.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Searchbarre from './components/Searchbarre';
+import Searchbarre from './Searchbarre';
 
 
 class Navbarre extends Component{
