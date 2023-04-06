646648e476b5f2fc761eabbb4b8898042470a194
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-06 17:03:39 +0200

    ..

---
+++ U3Reactweek2/g4/src/redux/reducers/index.js
@@ -0,0 +1,18 @@
+//file che crea reducer unico
+// funzione PURA che genera un output pari all input
+
+
+
+
+const initialState = {
+    lista : {
+        contentlist:[]
+    },
+}
+
+const mainreducer = (state = initialState, action) => {
+        //il reducer ha sempre uno switch case 
+        switch (action.type) {
+           
+        
+         
---
+++ U3Reactweek2/g4/src/redux/store/index.js
@@ -0,0 +1,5 @@
+import configurestore from "@reduxjs/toolkit"
+import mainreducer from "../reducers"
+
+const store = configurestore({
+    reducer:mainred
