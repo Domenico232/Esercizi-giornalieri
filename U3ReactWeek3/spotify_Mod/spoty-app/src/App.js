e6b7fea5e69bb2836deed039c7f3d4756dd76614
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-14 13:06:49 +0200

    m

--- U3ReactWeek3/spotify_Mod/spoty-app/src/App.js
+++ U3ReactWeek3/spotify_Mod/spoty-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Albump from './components/Albump';
 import './App.css';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+    <Route path='/' element={<Home />} /> 
+    <Route path='/album/' element={<Albump/>}/>
+      </Routes>
+      
     </div>
+ </BrowserRouter>
   );
 }
 
